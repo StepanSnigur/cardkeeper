bfaed3d03a485972b538d3c7b0e84c6180af6d23
StepanSnigur <snigur-stepan@mail.ru>
2021-07-15 20:02:20 +0300

    Add registration page

--- src/navigation/MenuNavigation.tsx
+++ src/navigation/MenuNavigation.tsx
@@ -4,6 +4,7 @@ import { useTheme } from 'react-native-paper'
 import Main from '../screens/Main'
 import SideMenu from '../screens/SideMenu'
 import Auth from '../screens/Auth'
+import Registration from '../screens/Registration'
 
 export type MenuNavigationParams = {
   Main: undefined
@@ -14,8 +15,12 @@ const MenuNavigation = () => {
   const { colors } = useTheme()
 
   return (
-    <Drawer.Navigator drawerContent={props => <SideMenu themeColors={colors} {...props} />}>
+    <Drawer.Navigator
+      initialRouteName="Авторизация"
+      drawerContent={props => <SideMenu themeColors={colors} {...props} />}
+    >
       <Drawer.Screen name="Авторизация" component={Auth} />
+      <Drawer.Screen name="Регистрация" component={Registration} />
       <Drawer.Screen name="Главная" component={Main} />
     </Drawer.Navigator>
   )
