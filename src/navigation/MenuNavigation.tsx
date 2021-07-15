c0c4415656abb59050f13067160cd4c0960346bf
StepanSnigur <snigur-stepan@mail.ru>
2021-07-15 19:16:04 +0300

    Add auth page

--- src/navigation/MenuNavigation.tsx
+++ src/navigation/MenuNavigation.tsx
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import { useTheme } from 'react-native-paper'
 import Main from '../screens/Main'
 import SideMenu from '../screens/SideMenu'
+import Auth from '../screens/Auth'
 
 export type MenuNavigationParams = {
   Main: undefined
@@ -14,6 +15,7 @@ const MenuNavigation = () => {
 
   return (
     <Drawer.Navigator drawerContent={props => <SideMenu themeColors={colors} {...props} />}>
+      <Drawer.Screen name="Авторизация" component={Auth} />
       <Drawer.Screen name="Главная" component={Main} />
     </Drawer.Navigator>
   )
