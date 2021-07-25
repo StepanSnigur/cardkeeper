2fe42c3ff5b1adadba92ccb5ce4a8a6e1d201b9e
StepanSnigur <snigur-stepan@mail.ru>
2021-07-25 16:35:43 +0300

    Add base layout

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Text, Avatar, useTheme } from 'react-native-paper'
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  avatar: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 60,
+  },
+  useEmail: {
+    textAlign: 'center',
+    marginTop: 20,
+    fontSize: 20,
+  },
+})
 
 const Profile = () => {
+  const { colors } = useTheme()
+
   return (
-    <Text>Profile</Text>
+    <View style={[styles.wrapper, { backgroundColor: colors.background }]}>
+      <Avatar.Image
+        style={styles.avatar}
+        source={{
+          uri: 'https://api.adorable.io/avatars/50/abott@adorable.png'
+        }}
+        size={120}
+      />
+      <Text style={styles.useEmail}>email@emali.com</Text>
+    </View>
   )
 }
 
