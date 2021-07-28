ccbea92dd32b9041894cbef72a6b2c8177c502f5
StepanSnigur <snigur-stepan@mail.ru>
2021-07-28 15:16:44 +0300

    Add control buttons

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Text, Avatar, useTheme } from 'react-native-paper'
+import { Text, Avatar, Button, useTheme } from 'react-native-paper'
 
 const styles = StyleSheet.create({
   wrapper: {
@@ -16,10 +16,26 @@ const styles = StyleSheet.create({
     marginTop: 20,
     fontSize: 20,
   },
+  controls: {
+    width: '90%',
+    marginTop: 'auto',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  controlButton: {
+    marginBottom: 15,
+  },
 })
 
 const Profile = () => {
   const { colors } = useTheme()
+
+  const handleChangeEmail = () => {
+    console.log('change email')
+  }
+  const handleChangePassword = () => {
+    console.log('change password')
+  }
 
   return (
     <View style={[styles.wrapper, { backgroundColor: colors.background }]}>
@@ -31,6 +47,18 @@ const Profile = () => {
         size={120}
       />
       <Text style={styles.useEmail}>email@emali.com</Text>
+      <View style={styles.controls}>
+        <Button
+          mode="contained"
+          style={styles.controlButton}
+          onPress={handleChangeEmail}
+        >Изменить Email</Button>
+        <Button
+          mode="contained"
+          style={styles.controlButton}
+          onPress={handleChangePassword}
+        >Изменить пароль</Button>
+      </View>
     </View>
   )
 }
