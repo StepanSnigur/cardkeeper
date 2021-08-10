bac2d7889a2270232723d02c59b8e946942795a8
Stepan Snigur <snigur-stepan@mail.ru>
2021-08-10 15:15:12 +0300

    Add settings tests

---
+++ __tests__/store/settings.test.ts
@@ -0,0 +1,39 @@
+import settings from '../../src/store/settings'
+// @ts-ignore
+import { setItem, getItem } from '@react-native-async-storage/async-storage'
+
+const defaultSettings = {
+  darkTheme: true,
+  fingerprint: false,
+}
+const mockUserSettings = {
+  darkTheme: false,
+  fingerprint: false,
+}
+
+jest.mock('@react-native-async-storage/async-storage', () => ({
+  getItem: jest.fn(),
+  setItem: jest.fn()
+}))
+
+describe('Settings store', () => {
+  beforeEach(() => {
+    setItem.mockImplementation(() => Promise.resolve(1))
+    getItem.mockImplementation(() => Promise.resolve(JSON.stringify(defaultSettings)))
+  })
+
+  it('Set correct settings by default', async () => {
+    await settings.getDefaultSettings()
+    expect(settings).toEqual(defaultSettings)
+  })
+  it('Correct set user settings', async () => {
+    getItem.mockImplementation(() => Promise.resolve(JSON.stringify(mockUserSettings)))
+    await settings.getDefaultSettings()
+    expect(settings).toEqual(mockUserSettings)
+  })
+  it('Correct change settings', async () => {
+    await settings.switchSetting('fingerprint')
+    expect(settings.fingerprint).toBe(true)
+    expect(setItem).toBeCalledTimes(1)
+  })
+})
