842b33148acb6328f1b1e250a530ffb221b0db90
Stepan Snigur <snigur-stepan@mail.ru>
2021-08-12 15:19:53 +0300

    Add auth validation

---
+++ src/utils/validator.ts
@@ -0,0 +1,15 @@
+class Validator {
+  private emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+  private invalidEmailMessage = 'Некорректный email'
+  private invalidPasswordMessage = 'Пароль должен быть длиннее 6 символов'
+
+  public validateEmail = (email: string): false | string => {
+    const isValid = email.match(this.emailRegex)
+    return isValid ? false : this.invalidEmailMessage
+  }
+  public validatePassword = (password: string): false | string => {
+    return password.length >= 6 ? false : this.invalidPasswordMessage
+  }
+}
+
+export default new Validator()
