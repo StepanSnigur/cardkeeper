cefd560c0b0bba7e2626d88e1ff54ea213d8714e
Stepan Snigur <snigur-stepan@mail.ru>
2021-08-28 18:30:57 +0300

    Add animation to alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { observer } from 'mobx-react-lite'
-import { View, StyleSheet } from 'react-native'
+import { StyleSheet, Animated } from 'react-native'
 import { Text } from 'react-native-paper'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import alert, { ALERT_COLORS } from '../store/alert'
@@ -24,18 +24,40 @@ const styles = StyleSheet.create({
 })
 
 const Alert = observer(() => {
+  const [isVisible, setIsVisible] = useState(false)
+  const slideUpAnimation = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    setIsVisible(true)
+    Animated.timing(slideUpAnimation, {
+      toValue: alert.alertMessage.length ? 1 : 0,
+      duration: 500,
+      useNativeDriver: false,
+    }).start(handleAnimationEnd)
+  }, [alert.alertMessage])
+
+  const handleAnimationEnd = () => {
+    !alert.alertMessage.length && setIsVisible(false)
+  }
+
   const truncatedMessage = alert.alertMessage.length > 30
     ? `${alert.alertMessage.slice(0, 30)}...`
     : alert.alertMessage
 
-  if (!alert.alertMessage.length) return null
+  if (!isVisible) return null
   return (
-    <View
-      style={[styles.wrapper, { backgroundColor: ALERT_COLORS[alert.alertType].color }]}
+    <Animated.View
+      style={[styles.wrapper, {
+        backgroundColor: ALERT_COLORS[alert.alertType].color,
+        bottom: slideUpAnimation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [-1000, 20],
+        })
+      }]}
     >
       <Icon name={ALERT_COLORS[alert.alertType].icon} size={20} />
       <Text style={styles.alertText}>{truncatedMessage}</Text>
-    </View>
+    </Animated.View>
   )
 })
 
